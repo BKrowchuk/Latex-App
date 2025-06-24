7dba6050fb853b59e8fc895bba58b37d052f88b3
benkrowchuk <162325063+benkrowchuk@users.noreply.github.com>
2025-06-24 13:48:12 -0400

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/Latex-App/",
+  // base: "/Latex-App/", // Commented out for local development
 });
